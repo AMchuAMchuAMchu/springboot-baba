88cdb862117a16562e6aae5d775f60b171c9eacb
AMchuAMchuAMchu <2466130840@qq.com>
2022-12-13 05:11:36 +0800

    shiina

--- springboot-ssm01/src/main/java/com/itcast/controller/UserController.java
+++ springboot-ssm01/src/main/java/com/itcast/controller/UserController.java
@@ -4,10 +4,12 @@ import com.itcast.dao.UserDao;
 import com.itcast.entity.User;
 import com.itcast.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -19,18 +21,20 @@ import java.util.List;
  * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
  */
 @RequestMapping("/user")
-@RestController
+@Controller
 public class UserController {
 
     @Autowired
     private UserDao userDao;
 
     @GetMapping
-    public String get(){
+    public String get(HttpServletResponse resp){
 
         List<User> users = userDao.selectList(null);
 
         users.forEach(System.out::println);
+
+
 
         return "index";
     }
