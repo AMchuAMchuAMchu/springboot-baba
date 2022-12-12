1184464108b117151b355762afb351bde982b591
AMchuAMchuAMchu <2466130840@qq.com>
2022-12-13 05:15:18 +0800

    shiina

--- springboot-ssm01/src/main/java/com/itcast/controller/UserController.java
+++ springboot-ssm01/src/main/java/com/itcast/controller/UserController.java
@@ -2,12 +2,10 @@ package com.itcast.controller;
 
 import com.itcast.dao.UserDao;
 import com.itcast.entity.User;
-import com.itcast.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
@@ -34,7 +32,7 @@ public class UserController {
 
         users.forEach(System.out::println);
 
-        return "index.html";
+        return "templates/index.html";
     }
 
 }
