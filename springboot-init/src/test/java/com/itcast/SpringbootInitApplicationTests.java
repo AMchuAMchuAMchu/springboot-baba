711f6885dd2b2c79be4f8a346f82803c658e6e43
AMchuAMchuAMchu <2466130840@qq.com>
2022-12-12 15:19:18 +0800

    shiina

---
+++ springboot-init/src/test/java/com/itcast/SpringbootInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
