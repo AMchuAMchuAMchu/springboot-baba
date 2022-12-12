23ed80fcb02e765f118e1b74e04f0a034738ce90
AMchuAMchuAMchu <2466130840@qq.com>
2022-12-13 05:16:34 +0800

    shiina

--- springboot-ssm01/src/main/java/com/itcast/controller/UserController.java
+++ springboot-ssm01/src/main/java/com/itcast/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
@@ -19,7 +20,7 @@ import java.util.List;
  * Author ==> _02雪乃赤瞳楪祈校条祭_艾米丽可锦木千束木更七草荠_制作委员会_start
  */
 @RequestMapping("/user")
-@Controller
+@RestController
 public class UserController {
 
     @Autowired
@@ -32,7 +33,7 @@ public class UserController {
 
         users.forEach(System.out::println);
 
-        return "templates/index.html";
+        return "<h1>ok</h1>";
     }
 
 }
