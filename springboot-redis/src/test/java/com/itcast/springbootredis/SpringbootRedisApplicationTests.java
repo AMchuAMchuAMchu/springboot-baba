a416a6c46446eeffdf2ef47c383c4e9c205fbb3d
AMchuAMchuAMchu <2466130840@qq.com>
2022-12-13 16:30:53 +0800

    shiina

--- springboot-redis/src/test/java/com/itcast/springbootredis/SpringbootRedisApplicationTests.java
+++ springboot-redis/src/test/java/com/itcast/springbootredis/SpringbootRedisApplicationTests.java
@@ -3,6 +3,7 @@ package com.itcast.springbootredis;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
@@ -16,9 +17,14 @@ class SpringbootRedisApplicationTests {
     @Test
     void contextLoads() {
 
-        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+//        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+//
+//        ops.set("k1","v1");
 
-        ops.set("k1","v1");
+        ListOperations<String, String> ops = redisTemplate.opsForList();
+
+        ops.leftPushAll("l1","l1","l2","l3","l3");
+
 
     }
 
